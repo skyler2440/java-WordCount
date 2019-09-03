4614bd6389cc5b10712e7aa0c8d00968bc139ba1
skyler dowdy <skylerwebdev@gmail.com>
2019-09-03 14:23:21 -0400

    words print to screen

---
+++ wc/src/wordCount/WordList.java
@@ -0,0 +1,21 @@
+package wordCount;
+
+public class WordList{
+
+	private String countList;
+
+	public WordList(String countList)
+	{
+		this.countList = countList;
+	}
+
+	public String getCountList()
+	{
+		return countList;
+	}
+
+	public void setCountList(String countList)
+	{
+		this.countList = countList;
+	}
+}
